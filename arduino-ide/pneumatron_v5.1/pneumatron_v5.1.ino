bb09c3961d4e4b4b6ec60639b452da7ea4c463f7
GabrielSlPires <gabriel.slpires@gmail.com>
2024-03-14 17:18:05 -0300

    organize setup

--- arduino-ide/pneumatron_v5.1/pneumatron_v5.1.ino
+++ arduino-ide/pneumatron_v5.1/pneumatron_v5.1.ino
@@ -26,48 +26,32 @@
   Code developed in Arduino IDE 2.3.2 enviroment
 */
 
-// For BMP280 sensor
-#include <Adafruit_BMP280.h>
-#include <Wire.h>
+#include <Wire.h> // I2C communication
+#include <Adafruit_BMP280.h> // BMP280 sensor
+#include <ESP8266WiFi.h> // Wireless communication
+#include <espnow.h> // Wireless communication
 
-// For wireless communication
-#include <ESP8266WiFi.h> 
-#include <espnow.h>
-
+// constant variables
 const int pneumatron = 106; // Pneumatron ID
+uint8_t broadcastAddress[] = {0xE8, 0xDB, 0x84, 0xCC, 0xB0, 0x64}; // receiver MAC Address
 
-// receiver MAC Address
-uint8_t broadcastAddress[] = {0xE8, 0xDB, 0x84, 0xCC, 0xB0, 0x64};
+// define peripherals 
+const int pump = 14;       // D5 (wemos)
+const int solenoid = 12;   // D6 (wemos)
+const int btn1 = 13;       // D7 (wemos)
+const int btn2 = 0;        // D3 (wemos)
+const int led1 = 15;       // D8 (wemos)
+const int led2 = 2;        // D4 (wemos)
 
-// define device modes
-enum DeviceMode {
-  AIR_DISCHARGE,
   VESSEL_LENGTH,
 };
-DeviceMode deviceMode = AIR_DISCHARGE; // Start with AIR_DISCHARGE
 
-// create button states to program change_mode()
-enum ButtonState {
+enum ButtonState { // define button states to code easily
   UP,
   DOWN
 };
 
-// define peripherals 
-int pump = 14;       // D5 (wemos)
-int solenoid = 12;   // D6 (wemos)
-int btn1 = 13;       // D7 (wemos)
-int btn2 = 0;        // D3 (wemos)
-int led1 = 15;       // D8 (wemos)
-int led2 = 2;        // D4 (wemos)
-
-// For BMP280
-float pressure; 
-Adafruit_BMP280 bmp;
-
-ADC_MODE(ADC_VCC);  // to measure voltage
-
-// data structure for wireless communication
-struct {
+struct { // data structure for wireless communication
   int id;
   int group;
   int sequence;
@@ -77,29 +61,23 @@ struct {
   float pressure;
   float temp;
   float v;
-  String version = "v3"; // se usar o novo jeito de fazer o ad, tem q ser v4
+  String version; // vou usar aqui para reportar algum erro
+  //String version = "v3"; // se usar o novo jeito de fazer o ad, tem q ser v4
 } data; // adicionar uma nova coluna, mode: ad ou vld
 
-// Identify communication problems
-void OnDataSent(uint8_t *mac_addr, uint8_t sendStatus) {
-Serial.print("\r\nLast Packet Send Status: ");
-  if (sendStatus == 0){
-    Serial.println("Delivery success");
-  }
-  else{
-    Serial.println("Delivery fail");
-    // add red LED
-  }
-}
+Adafruit_BMP280 bmp; // For BMP280
+
+ADC_MODE(ADC_VCC);  // to measure voltage
+
+// functions prototypes
+
 
 void setup() {
+  // initialize serial communication
   Serial.begin(115200);
   Serial.println("Starting Device...");
 
-  if (!bmp.begin(0x76)) {
-    Serial.println("Failed to initialize BMP280");
-  }
-  
+  // initialize digital I/O
   pinMode(pump, OUTPUT);
   pinMode(solenoid, OUTPUT);
   pinMode(btn1, INPUT_PULLUP); 
@@ -108,11 +86,10 @@ void setup() {
   pinMode(led2, OUTPUT);
 
   // Set device as a Wi-Fi Station
-  WiFi.mode(WIFI_STA);
+  WiFi.mode(WIFI_STA); 
   WiFi.disconnect();
- 
   // Init ESP-NOW
-  if (esp_now_init() != 0) {
+  while (esp_now_init() != 0) {
     Serial.println("Error initializing ESP-NOW");
     return;
   } 
@@ -127,10 +104,12 @@ void setup() {
   // Register peer
   esp_now_add_peer(broadcastAddress, ESP_NOW_ROLE_SLAVE, 1, NULL, 0);
 
+  data.version = "v3"; // remover depois
+
+  // Custom actions to finish setup
   data.id = pneumatron;
-
+  data.humid = 0; // BMP280 do not measures humid
   randomSeed(micros() + pneumatron); //Start random generator
-
   startUpFlash();
 }
 
