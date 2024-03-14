d0273123e4a4dfc3bc1be11bb20244c4937e41e8
GabrielSlPires <gabriel.slpires@gmail.com>
2024-03-14 17:21:54 -0300

    separete communication function

---
+++ arduino-ide/pneumatron_v5.1/communication.ino
@@ -0,0 +1,11 @@
+// Identify communication problems
+void OnDataSent(uint8_t *mac_addr, uint8_t sendStatus) {
+Serial.print("\r\nLast Packet Send Status: ");
+  if (sendStatus == 0){
+    Serial.println("Delivery success");
+  }
+  else{
+    Serial.println("Delivery fail");
+    // add red LED
+  }
+}
