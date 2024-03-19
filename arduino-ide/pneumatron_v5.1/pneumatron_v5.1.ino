9f0ad2e28811870dee7cafef947f969830460d32
GabrielSlPires <gabriel.slpires@gmail.com>
2024-03-19 02:25:04 -0300

    changedbmp filtering to OFF because vaccum pump is our "noise"

--- arduino-ide/pneumatron_v5.1/pneumatron_v5.1.ino
+++ arduino-ide/pneumatron_v5.1/pneumatron_v5.1.ino
@@ -98,7 +98,7 @@ void setup() {
   bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     // Operating Mode
                   Adafruit_BMP280::SAMPLING_X2,     // Temp. oversampling
                   Adafruit_BMP280::SAMPLING_X16,    // Pressure oversampling
-                  Adafruit_BMP280::FILTER_X16 ,     // Filtering
+                  Adafruit_BMP280::FILTER_OFF,      // Filtering, changed to OFF because vaccum pump is our "noise"
                   Adafruit_BMP280::STANDBY_MS_1);   // Standby time
 
   // initialize Wifi communication
